d0c7c4845c49d94a36882e3f68a16085d804e690
ricardovejarano10 <ricardo.vejarano.garcia@gmail.com>
2018-09-24 15:30:17 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Neo Mundo Travel
+
+Azar Game for clients
+
+## Basic Usage
+
+```shell
+git clone
+cd angular-electron
+npm install
+
+# run the app
+npm run electron-build
+
+# build the app
+npm install electron-packager -g
+electron-packager . --platform=win32
+```
---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/takeWhile';
+import 'rxjs/add/operator/do';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  
+}
