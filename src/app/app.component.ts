fe3ff8063451cb87d0f665f7faa402e677e48e43
ricardovejarano10 <ricardo.vejarano.garcia@gmail.com>
2018-09-24 22:37:04 -0500

    Input focus ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
+  @ViewChild('In1') input1: ElementRef;
+  @ViewChild('In2') input2: ElementRef;
+  @ViewChild('In3') input3: ElementRef;
+  @ViewChild('In4') input4: ElementRef;
+  @ViewChild('In5') input5: ElementRef;
+
   constructor() {
-    
+
   }
 
   ngOnInit() {
 
   }
-  
+
+  onSearchChange(value: number) {
+    switch (value) {
+      case 0:
+      console.log('Entra', value);
+        this.input2.nativeElement.focus();
+        break;
+      case 1:
+        this.input3.nativeElement.focus();
+        break;
+      case 2:
+        this.input4.nativeElement.focus();
+        break;
+      case 3:
+        this.input5.nativeElement.focus();
+        break;
+      case 4:
+        break;
+
+    }
+  }
+
+  play() {
+    console.log('Se sortea al juego');
+  }
+
 }
